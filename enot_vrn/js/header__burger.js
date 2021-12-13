91a1257f60fb40d0e63a55bb2ffa6a2661dc2f30
PavDev <68286435+JetPhyton@users.noreply.github.com>
2021-12-13 22:44:19 +0300

    Delete enot_vrn directory

--- enot_vrn/js/header__burger.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.header__burger').onclick = function () {
-  // Активирует анимацию бургера
-  document.querySelector('.header__burger').classList.toggle("active");
-  // Включает меню
-  document.querySelector('.burger_menu').classList.toggle("active");
-  document.querySelector('body').classList.toggle("lock");
-}
--- enot_vrn/js/popup.js
+++
@@ -1,9 +0,0 @@
-if (e.target.classList == "popup") {
-  var popUp = document.getElementsByClassName('popup');
-  var popUpContent = document.getElementsByClassName('popup__text')
-  for (var i = 0; i < popUp.length; i++) {
-    if (e.target.classList == popUp[i].classList) {
-      popUpContent[i].classList.toggle("active");
-    }
-  }
-}
